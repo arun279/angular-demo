d9261756646fc2e340c55580ed38de6b019bc94e
Arun Krishnamurthy <infinite279@gmail.com>
2019-12-03 16:41:58 -0500

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component3.html',
+  // template: `<div class="error">Error</div>`,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  showMessage = true;
+  counterInterval;
+  ngOnInit(): void {
+    let counter = 0;
+    this.counterInterval = setInterval(function() {
+      counter = counter + 1;
+      if (counter % 2 == 0) {
+        this.showMessage = true;
+      }
+      else {
+        this.showMessage = false;
+      }
+    }, 1000)
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.counterInterval);
+  }
+
+
+  // // old code
+  // title = 'angular-demo';
+  // //model
+  // name = "valar morghulis"
+  // color = "black"
+  // //logic
+  // handleButtonClick() {
+  //   if(this.name == "valar morghulis") {
+  //     this.name = "valar dohaeris";
+  //   }
+  //   else {
+  //     this.name = "valar morghulis";
+  //   }
+  // }
+
+  // changeColor(color) {
+  //   this.color = color
+  // }
+}

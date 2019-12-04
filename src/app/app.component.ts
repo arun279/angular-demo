77f53db12c135fc151c81c2871af86af950165cc
Arun Krishnamurthy <infinite279@gmail.com>
2019-12-04 10:17:27 -0500

    added header component
    commented out old code, added header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,47 +2,56 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component3.html',
+  templateUrl: './app.component2.html',
   // template: `<div class="error">Error</div>`,
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit {
-  showMessage = true;
-  counterInterval;
   ngOnInit(): void {
-    let counter = 0;
-    this.counterInterval = setInterval(function() {
-      counter = counter + 1;
-      if (counter % 2 == 0) {
-        this.showMessage = true;
-      }
-      else {
-        this.showMessage = false;
-      }
-    }, 1000)
+    throw new Error("Method not implemented.");
   }
 
-  ngOnDestroy() {
-    clearInterval(this.counterInterval);
-  }
-
-
-  // // old code
-  // title = 'angular-demo';
-  // //model
-  // name = "valar morghulis"
-  // color = "black"
-  // //logic
-  // handleButtonClick() {
-  //   if(this.name == "valar morghulis") {
-  //     this.name = "valar dohaeris";
-  //   }
-  //   else {
-  //     this.name = "valar morghulis";
-  //   }
-  // }
-
-  // changeColor(color) {
-  //   this.color = color
-  // }
 }
+
+// export class AppComponent implements OnInit {
+//   showMessage = true;
+//   counterInterval;
+//   ngOnInit(): void {
+//     let counter = 0;
+//     this.counterInterval = setInterval(function() {
+//       counter = counter + 1;
+//       if (counter % 2 == 0) {
+//         this.showMessage = true;
+//       }
+//       else {
+//         this.showMessage = false;
+//       }
+//     }, 1000)
+//   }
+
+//   ngOnDestroy() {
+//     clearInterval(this.counterInterval);
+//   }
+// }
+
+// export class AppComponent implements OnInit {
+//   // old code
+//   title = 'angular-demo';
+//   //model
+//   name = "valar morghulis"
+//   color = "black"
+//   //logic
+//   handleButtonClick() {
+//     if(this.name == "valar morghulis") {
+//       this.name = "valar dohaeris";
+//     }
+//     else {
+//       this.name = "valar morghulis";
+//     }
+//   }
+
+//   changeColor(color) {
+//     this.color = color
+//   }
+// }
